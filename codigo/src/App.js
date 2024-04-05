8e7c5f046cca0a788d9416f395f90bdfc48e3e89
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-04-05 02:03:01 -0500

    routes

--- codigo/src/App.js
+++ codigo/src/App.js
@@ -1,14 +1,18 @@
 import MiGrafico from './graficas/MiGrafico';
 import MiGrafico2 from './graficas/MiGrafico2';
+import Header from './reuse/header';
 function App() {
   return (
-    <div className="container text-center">
-      <div className='row'>        <h1>Mr. Homero</h1>
-        <div className='col-6'>
-          <MiGrafico></MiGrafico>
-        </div> 
-        <div className='col-6'>
-          <MiGrafico2></MiGrafico2>
+    <div className="container-fluid text-center">
+      <Header></Header>
+      <div className='container'>
+        <div className='row'>  <h1>Mr. Homero</h1>
+          <div className='col-6'>
+            <MiGrafico></MiGrafico>
+          </div> 
+          <div className='col-6'>
+            <MiGrafico2></MiGrafico2>
+          </div>
         </div>
       </div>
     </div>
