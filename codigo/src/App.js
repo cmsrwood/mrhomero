78eeb97b14f934d7c3abb62df255e882b2b7ed24
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-04-05 17:18:41 -0500

    routes react

--- codigo/src/App.js
+++ codigo/src/App.js
@@ -1,20 +1,16 @@
+import {  BrowserRouter,  Routes,  Route} from "react-router-dom";
+import Navegacion from './Plantilla/Navegacion';
 import MiGrafico from './graficas/MiGrafico';
 import MiGrafico2 from './graficas/MiGrafico2';
-import Header from './reuse/header';
 function App() {
   return (
     <div className="container-fluid text-center">
-      <Header></Header>
-      <div className='container'>
-        <div className='row'>  <h1>Mr. Homero</h1>
-          <div className='col-6'>
-            <MiGrafico></MiGrafico>
-          </div> 
-          <div className='col-6'>
-            <MiGrafico2></MiGrafico2>
-          </div>
-        </div>
-      </div>
+      <BrowserRouter>
+      <Navegacion/>
+        <Routes>
+          <Route path='/' element={<MiGrafico/>}> </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
