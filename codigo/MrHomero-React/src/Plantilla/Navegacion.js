8722b8fec626119a47c0282c14f4c3f1f6cae27d
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-04-15 15:48:39 -0500

    nav

--- codigo/MrHomero-React/src/Plantilla/Navegacion.js
+++ codigo/MrHomero-React/src/Plantilla/Navegacion.js
@@ -6,23 +6,17 @@ export default function Navegacion() {
         <div>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Mr. Homero</a>
+                    <Link class="navbar-brand" to="/">Mr.Homero</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link class="nav-link" to="/">Inicio</Link>
                             </li>
                             <li class="nav-item">
-                                <Link class="nav-link" to="/SegundaGrafica">Segunda grafica</Link>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                                <Link class="nav-link" to="/ruta">Ruta ejemplo</Link>
                             </li>
                         </ul>
                     </div>
