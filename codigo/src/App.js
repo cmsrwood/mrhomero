7c860e0ea3619274ec3c7e3de41b069df2813c11
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-04-14 20:27:33 -0500

    chart js

--- codigo/src/App.js
+++ codigo/src/App.js
@@ -1,7 +1,6 @@
 import {  BrowserRouter,  Routes,  Route} from "react-router-dom";
 import Navegacion from './Plantilla/Navegacion';
 import MiGrafico from './graficas/MiGrafico';
-import MiGrafico2 from './graficas/MiGrafico2';
 function App() {
   return (
     <div className="container-fluid text-center">
