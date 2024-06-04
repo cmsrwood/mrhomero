9682bf722f58601c45b16b7e9a82b6edcfa8c9f4
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-06-04 15:50:58 -0500

    img

--- codigo/MrHomero-React/src/App.jsx
+++ codigo/MrHomero-React/src/App.jsx
@@ -1,13 +1,14 @@
 import {  BrowserRouter,  Routes,  Route} from "react-router-dom";
 import NavegacionDefault from "./Plantilla/NavegacionDefault";
-import MiGrafico from "./graficas/MiGrafico"
+import MiGrafico from "./Graficas/MiGrafico"
+import Index from "./Pages/default/Index";
 function App() {
   return (
     <div className="text-center">
       <BrowserRouter>
       <NavegacionDefault/>
         <Routes>
-          <Route exact path='/' element={<MiGrafico/>}> </Route>
+          <Route exact path='/' element={<Index/>}> </Route>
         </Routes>
       </BrowserRouter>
     </div>
