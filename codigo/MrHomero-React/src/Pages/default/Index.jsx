7dc7577a04dd6b582461df3c29e9f3a5e18a5b5d
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-06-04 17:23:47 -0500

    index.jsx y nuevas carpetas

--- codigo/MrHomero-React/src/Pages/default/Index.jsx
+++ codigo/MrHomero-React/src/Pages/default/Index.jsx
@@ -5,26 +5,26 @@ export default function Index() {
   return (
     <div className=''>
       <div id="carouselExample" class="carousel slide ">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src={logo} class="d-block w-50" alt="..."/>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={logo} class="d-block w-50" alt="..."/>
+          </div>
+          <div class="carousel-item">
+            <img src={logo} class="d-block w-50" alt="..."/>
+          </div>
+          <div class="carousel-item">
+            <img src={logo} class="d-block w-50" alt="..."/>
+          </div>
         </div>
-        <div class="carousel-item">
-          <img src={logo} class="d-block w-50" alt="..."/>
-        </div>
-        <div class="carousel-item">
-          <img src={logo} class="d-block w-50" alt="..."/>
-        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
     </div>
   )
 }
