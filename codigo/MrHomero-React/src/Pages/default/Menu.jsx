1fadd955b0d71622ebb84c72fbb9f0003416ccdc
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-06-07 17:29:30 -0500

    swiper funcional

--- codigo/MrHomero-React/src/Pages/default/Menu.jsx
+++ codigo/MrHomero-React/src/Pages/default/Menu.jsx
@@ -6,63 +6,56 @@ import 'swiper/css/bundle';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import 'swiper/css/effect-creative';
+import img from './img/img.png';
 
 export default function Menu() {
 
     const swiper = useSwiper();
 
 
-    function cards(times) {
-        const cards = [];
-        for (let i = 0; i < times; i++) {
-            cards.push(
-                <div className="card text-center" key={i}>
-                    <div className="card-body">
-                        <h5 className="card-title">Special title treatment</h5>
-                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
+  
+    function card () {
+        return (
+            <div className="card text-center">
+                <img src={img} height={200} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Hamburguesa</h5>
+                    <p className="card-text">$10.000</p>
+                    <a href="#" className="btn btn-primary">Ver</a>
                 </div>
-            );
-        }
-        return cards;
+            </div>
+        );
     }
+        
     return (
         <div className="container">
             <h1 className="text-warning text-center p-2">Menu</h1>
-            <div>
+            <h2 className="p-2 mb-4">Categoria</h2>
                 <Swiper
                     grabCursor={true}
                     effect={'creative'}
                     creativeEffect={{
                         prev: {
-                            shadow: true,
-                            translate: ['-120%', 0, -500],
+                            translate: ['-100%', 0, -500],
                         },
                         next: {
-                            shadow: true,
-                            translate: ['120%', 0, -500],
+                            translate: ['100%', 0, -500],
                         },
                     }}
+                    centeredSlides={true}
                     modules={[Navigation, Pagination, EffectCreative ]}
                     navigation={{ clickable: true }}
                     pagination={{ clickable: true }}
                     loop={true}
                     slidesPerView={3}
-                    spaceBetween={10}
                     className="mySwiper2"
                 >
-                    <SwiperSlide>{cards(1)} </SwiperSlide>
-                    <SwiperSlide>{cards(1)} </SwiperSlide>
-                    <SwiperSlide>{cards(1)} </SwiperSlide>
-                    <SwiperSlide>{cards(1)} </SwiperSlide>
-                    <SwiperSlide>{cards(1)} </SwiperSlide>
-                    <SwiperSlide>{cards(1)} </SwiperSlide>
-                    <SwiperSlide>{cards(1)} </SwiperSlide>
-                    <SwiperSlide>{cards(1)} </SwiperSlide>
-                    <SwiperSlide>{cards(1)} </SwiperSlide>
+                    <SwiperSlide>{card()} </SwiperSlide>
+                    <SwiperSlide>{card()} </SwiperSlide>
+                    <SwiperSlide>{card()} </SwiperSlide>
+                    <SwiperSlide>{card()} </SwiperSlide>
+                    <SwiperSlide>{card()} </SwiperSlide>
                 </Swiper>
-            </div>
         </div>
     )
 }
