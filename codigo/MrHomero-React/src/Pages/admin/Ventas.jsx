3ba40475d7f018e20e1b700c9bb90366a6f13601
johansmc <163949679+johansmc@users.noreply.github.com>
2024-07-16 13:44:24 -0500

    Creacion de componente Ventas

--- codigo/MrHomero-React/src/Pages/admin/Ventas.jsx
+++ codigo/MrHomero-React/src/Pages/admin/Ventas.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import Buscador from '../../Plantilla/Buscador'
 
 export default function Ventas() {
   return (
-    <div>
-      
+    <div className=''>
+      <div className=" container p-5">
+        <div className="row">
+          <h1 className="col-12 col-sm-6">Gestion de ventas</h1>
+          <div className="col-12 col-sm-6 position-relative">
+           <Buscador icon="search" placeholder="Buscar"/>
+          </div>
+        </div>
+        <table className=" table table-striped mt-5">
+          <thead>
+            <tr>
+              <th scope="col">id</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
---
+++ codigo/MrHomero-React/src/Plantilla/Buscador.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Buscador({icon, placeholder}) {
+    return (
+        <div className="search-input position-relative">
+            <input type="search" class="form-control form-control-lg ps-5" placeholder={placeholder} />
+            <i className= {`bi bi-${icon} position-absolute top-50 start-0 translate-middle-y ms-3 text-secondary`}></i>
+        </div>
+    )
+}
