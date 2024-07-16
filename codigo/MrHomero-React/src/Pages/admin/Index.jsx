29e0761a0b5cb860fd0d65065eba9f7555f44a2f
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-07-16 11:44:48 -0500

    Funciones react para admin

---
+++ codigo/MrHomero-React/src/Pages/admin/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function IndexAdmin() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- codigo/MrHomero-React/src/Pages/admin/MenuAdmin.jsx
+++ codigo/MrHomero-React/src/Pages/admin/MenuAdmin.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Pedidos() {
+export default function MenuAdmin() {
   return (
     <div>
       
