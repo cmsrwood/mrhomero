b0e5d60949e70f140eb06c44c68e8f5d628ea5fa
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-07-17 08:05:27 -0500

    navegacion admin

--- codigo/MrHomero-React/src/Pages/admin/PlantillaAdmin/NavegacionAdmin.jsx
+++ codigo/MrHomero-React/src/Pages/admin/PlantillaAdmin/NavegacionAdmin.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+import Ventas from '../Ventas'
 
 export default function NavegacionAdmin() {
     function tema() {
@@ -16,31 +18,28 @@ export default function NavegacionAdmin() {
     }
     return (
         <div className="d-flex">
-            <div className='border-end bg-dark min-vh-100 px-5'>
-                <h1 className="fs-4 pt-3 pb-4">Mr. Homero</h1>
-                <ul className="list-unstyled px-2">
-                    <li className="active"><a href="#" className="text-decoration-none px-3 py-2 d-block"><i
-                        className="bi bi-house"></i> Inicio</a></li>
-                    <li className=""><a href="#" className="text-decoration-none px-3 py-2 d-block"><i className="fal fa-list"></i>
-                        Projects</a></li>
-                    <li className=""><a href="#" className="text-decoration-none px-3 py-2 d-block d-flex">
-                        <span><i className="fal fa-comment"></i> Messages</span>
+            <div className='border-end min-vh-100 pe-5'>
+                <h1 className="fs-4 pt-3 pb-4 text-center">Mr. Homero</h1>
+                <ul className="px-2">
+                    <a href="#" className="text-decoration-none px-3 py-2 d-block">
+                        <i className="bi bi-house"></i> Inicio
                     </a>
-                    </li>
-                    <li className=""><a href="#" className="text-decoration-none px-3 py-2 d-block"><i
-                        className="fal fa-envelope-open-text"></i> Services</a></li>
-                    <li className=""><a href="#" className="text-decoration-none px-3 py-2 d-block"><i className="fal fa-users"></i>
-                        Customers</a></li>
+                    <a href="#" className="text-decoration-none px-3 py-2 d-block">
+                        <i className="bi bi-house"></i> Inicio
+                    </a>
+                    <a href="#" className="text-decoration-none px-3 py-2 d-block">
+                        <i className="bi bi-house"></i> Inicio
+                    </a>
                 </ul>
                 <ul className="list-unstyled px-2">
                     <li className=""><a href="#" className="text-decoration-none px-3 py-2 d-block"><i className="fal fa-bars"></i>
-                        Settings</a></li>
+                        Configuracion</a></li>
                     <li className=""><a href="#" className="text-decoration-none px-3 py-2 d-block"><i className="fal fa-bell"></i>
-                        Notifications</a></li>
+                        Perfil</a></li>
                 </ul>
             </div>
             <div className="w-100">
-                <nav className="navbar navbar-expand-lg border-bottom bg-black px-5">
+                <nav className="navbar navbar-expand-lg border-bottom px-5">
                     <div className="container-fluid">
                         <Link className="navbar-brand" to="/">Mr.Homero</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -69,10 +68,7 @@ export default function NavegacionAdmin() {
                 </nav>
                 {/* Contenido */}
                 <div className="px-3 pt-4">
-                    <h2 className="fs-5"> Dashboard</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, totam? Sequi alias eveniet ut quas
-                        ullam delectus et quasi incidunt rem deserunt asperiores reiciendis assumenda doloremque provident,
-                        dolores aspernatur neque.</p>
+                    <Ventas/>
                 </div>
             </div>
         </div>
