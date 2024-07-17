184730a828f8eb262facaa77a7f7e401556276b8
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-07-16 22:22:45 -0500

    navegacion admin

--- codigo/MrHomero-React/src/Pages/admin/PlantillaAdmin/NavegacionAdmin.jsx
+++ codigo/MrHomero-React/src/Pages/admin/PlantillaAdmin/NavegacionAdmin.jsx
@@ -15,75 +15,66 @@ export default function NavegacionAdmin() {
         }
     }
     return (
-        <div class="d-flex">
-        <div class="sidebar" id="side_nav">
-            <div class="header-box px-2 pt-3 pb-4 d-flex justify-content-between">
-                <h1 class="fs-4"><span class="bg-white text-dark rounded shadow px-2 me-2">CL</span> <span
-                        class="text-white">Coding League</span></h1>
-                <button class="btn d-md-none d-block close-btn px-1 py-0 text-white"><i
-                        class="fal fa-stream"></i></button>
-            </div>
-
-            <ul class="list-unstyled px-2">
-                <li class="active"><a href="#" class="text-decoration-none px-3 py-2 d-block"><i
-                            class="fal fa-home"></i> Dashboard</a></li>
-                <li class=""><a href="#" class="text-decoration-none px-3 py-2 d-block"><i class="fal fa-list"></i>
+        <div className="d-flex">
+            <div className='border-end bg-dark min-vh-100 px-5'>
+                <h1 className="fs-4 pt-3 pb-4">Mr. Homero</h1>
+                <ul className="list-unstyled px-2">
+                    <li className="active"><a href="#" className="text-decoration-none px-3 py-2 d-block"><i
+                        className="bi bi-house"></i> Inicio</a></li>
+                    <li className=""><a href="#" className="text-decoration-none px-3 py-2 d-block"><i className="fal fa-list"></i>
                         Projects</a></li>
-                <li class=""><a href="#" class="text-decoration-none px-3 py-2 d-block d-flex justify-content-between">
-                        <span><i class="fal fa-comment"></i> Messages</span>
-                        <span class="bg-dark rounded-pill text-white py-0 px-2">02</span>
+                    <li className=""><a href="#" className="text-decoration-none px-3 py-2 d-block d-flex">
+                        <span><i className="fal fa-comment"></i> Messages</span>
                     </a>
-                </li>
-                <li class=""><a href="#" class="text-decoration-none px-3 py-2 d-block"><i
-                            class="fal fa-envelope-open-text"></i> Services</a></li>
-                <li class=""><a href="#" class="text-decoration-none px-3 py-2 d-block"><i class="fal fa-users"></i>
+                    </li>
+                    <li className=""><a href="#" className="text-decoration-none px-3 py-2 d-block"><i
+                        className="fal fa-envelope-open-text"></i> Services</a></li>
+                    <li className=""><a href="#" className="text-decoration-none px-3 py-2 d-block"><i className="fal fa-users"></i>
                         Customers</a></li>
-            </ul>
-            <ul class="list-unstyled px-2">
-                <li class=""><a href="#" class="text-decoration-none px-3 py-2 d-block"><i class="fal fa-bars"></i>
+                </ul>
+                <ul className="list-unstyled px-2">
+                    <li className=""><a href="#" className="text-decoration-none px-3 py-2 d-block"><i className="fal fa-bars"></i>
                         Settings</a></li>
-                <li class=""><a href="#" class="text-decoration-none px-3 py-2 d-block"><i class="fal fa-bell"></i>
+                    <li className=""><a href="#" className="text-decoration-none px-3 py-2 d-block"><i className="fal fa-bell"></i>
                         Notifications</a></li>
-
-            </ul>
-
-        </div>
-        <div class="w-100">
-            <nav className="navbar navbar-expand-lg bg-body-tertiary px-5">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">Mr.Homero</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarText">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        </ul>
-                        <small>Hola, Administrador</small>
-                        <div class="dropdown pe-5">
-                        <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="bi bi-person-square"></i>
-                            </button>
-                            <button class="btn btn-primary d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">
-                                <i class="bi bi-person-square"></i>
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </div>
+            <div className="w-100">
+                <nav className="navbar navbar-expand-lg border-bottom bg-black px-5">
+                    <div className="container-fluid">
+                        <Link className="navbar-brand" to="/">Mr.Homero</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarText">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             </ul>
+                            <small>Hola, Administrador</small>
+                            <div className="dropdown pe-5">
+                                <button className="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i className="bi bi-person-square"></i>
+                                </button>
+                                <button className="btn btn-primary d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">
+                                    <i className="bi bi-person-square"></i>
+                                </button>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </div>
+                            <button className='btn' onClick={tema}><i id="botont" className='bi bi-moon-fill'></i></button>
                         </div>
-                        <button className='btn' onClick={tema}><i id="botont" className='bi bi-moon-fill'></i></button>
                     </div>
+                </nav>
+                {/* Contenido */}
+                <div className="px-3 pt-4">
+                    <h2 className="fs-5"> Dashboard</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, totam? Sequi alias eveniet ut quas
+                        ullam delectus et quasi incidunt rem deserunt asperiores reiciendis assumenda doloremque provident,
+                        dolores aspernatur neque.</p>
                 </div>
-            </nav>
-
-            <div class="dashboard-content px-3 pt-4">
-                <h2 class="fs-5"> Dashboard</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, totam? Sequi alias eveniet ut quas
-                    ullam delectus et quasi incidunt rem deserunt asperiores reiciendis assumenda doloremque provident,
-                    dolores aspernatur neque.</p>
             </div>
         </div>
-    </div>
     )
 }
