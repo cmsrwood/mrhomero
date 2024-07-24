3ed07312e1ce481284fc1dfb0476e94a89a0feab
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-07-24 10:48:29 -0500

    grafica a plantillas

--- codigo/MrHomero-React/src/Pages/admin/Dashboard.jsx
+++ codigo/MrHomero-React/src/Pages/admin/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CustomChart from '../../graficas/CustomChart'
+import CustomChart from '../../Plantilla/CustomChart';
 export default function Dashboard() {
   const data = {
     labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
@@ -20,6 +20,7 @@ export default function Dashboard() {
         beginAtZero: true,
       },
     },
+    
   };
   return (
     <div className='container pt-3'>
@@ -27,12 +28,12 @@ export default function Dashboard() {
         <div className="text-center justify-content-center">
           <CustomChart data={data} tipo='line' options={options} />
         </div>
-        <div className="col-12 col-sm border border-2 mx-5 border-secondary  text-center">
+        <div className="col-12 col-sm border border-2 mx-0 mx-sm-5 border-secondary  text-center">
           <h3 className='pt-4'>Prductos vendidos</h3>
           <p className='pt-2'>1234 unidades</p>
           <h4 className='pb-4 text-success'>+ 8% este mes</h4>
         </div>
-        <div className="col-12 col-sm border mx-5 border-2 border-secondary text-center">
+        <div className="col-12 col-sm border mx-0 mx-sm-5 border-2 border-secondary text-center">
           <h3 className='pt-4'>Total ganancias</h3>
           <h4 className='pt-2'>COP 10.000.000</h4>
           <h4 className='pb-4 text-danger'>-5 % este mes</h4>
