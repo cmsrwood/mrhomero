74ea7ebe90db9c6b415e26e483006ba400525755
Abjura <bryamccuervo2004@gmail.com>
2024-07-25 18:11:37 -0500

    Scrollbar en pedidos del admin

--- codigo/MrHomero-React/src/Pages/admin/Pedidos.jsx
+++ codigo/MrHomero-React/src/Pages/admin/Pedidos.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Pagination } from 'swiper/modules'
+import { Pagination, Scrollbar } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 import '../../css/style.css'
-import img from '/img.png';
+import img from '/img.png'
 
 export default function Pedidos() {
   return (
@@ -14,19 +15,20 @@ export default function Pedidos() {
         slidesPerView={4}
         spaceBetween={10}
         pagination={{
-          clickable: true
+          clickable: true,
         }}
-        modules={{ Pagination }}
+        scrollbar={{ hide: true }}
+        modules={[Scrollbar, Pagination]}
         className="mySwiper p-2 w-100 ms-5"
       >
         <SwiperSlide><img className="card-img w-50 h-100"src={img}></img></SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide><img className="card-img w-50 h-100"src={img}></img></SwiperSlide>
+        <SwiperSlide><img className="card-img w-50 h-100"src={img}></img></SwiperSlide>
+        <SwiperSlide><img className="card-img w-50 h-100"src={img}></img></SwiperSlide>
+        <SwiperSlide><img className="card-img w-50 h-100"src={img}></img></SwiperSlide>
+        <SwiperSlide><img className="card-img w-50 h-100"src={img}></img></SwiperSlide>
+        <SwiperSlide><img className="card-img w-50 h-100"src={img}></img></SwiperSlide>
+        <SwiperSlide><img className="card-img w-50 h-100"src={img}></img></SwiperSlide>
       </Swiper>
     </div>
   )
