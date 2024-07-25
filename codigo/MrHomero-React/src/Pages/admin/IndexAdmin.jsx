4861298e601ac1d6c12cfdc211218263cafff604
Abjura <bryamccuervo2004@gmail.com>
2024-07-25 13:34:05 -0500

    Error en las cartas de las categorias en productos

--- codigo/MrHomero-React/src/Pages/admin/IndexAdmin.jsx
+++ codigo/MrHomero-React/src/Pages/admin/IndexAdmin.jsx
@@ -5,14 +5,14 @@ export default function IndexAdmin() {
   return (
     <div className='container'>
       <div className='text-center pb-3 pt-3'>
-            <h1>Mr. Homero | Inicio</h1>
+        <h1>Mr. Homero | Inicio</h1>
       </div>
       <div className='container border border-2 border-secondary p-3 mb-2'>
         <h3 className='m-3'>Bienvenido (usuario)</h3>
         <p className='m-3'>Estas son algunas de las funcuiones más utilizadas por tí</p>
         <div className='row aling items-center m-2'>
           <div className='col'>
-            <h5>Ventas</h5> 
+            <h5>Ventas</h5>
             <Link className='d-block mx-1'>Prueba</Link>
             <Link className='d-block mx-1'>Prueba</Link>
             <Link className='d-block mx-1'>Prueba</Link>
--- codigo/MrHomero-React/src/Pages/admin/Pedidos.jsx
+++ codigo/MrHomero-React/src/Pages/admin/Pedidos.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import '../../css/style.css'
 
 export default function Pedidos() {
   return (
-    <div>
-      
+    <div className='container p-3 border border-2 border-secondary'>
+      <h1>Pedidos</h1>
+      <div className='border border-2 border-secondary'>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={10}
+        pagination={{
+          clickable: true
+        }}
+        modules={{ Pagination }}
+        className="mySwiper container position-absolute top-0 end-0"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+      </Swiper>
+      </div>
     </div>
   )
 }
