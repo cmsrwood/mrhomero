9266a23b5b4d6be0444a94a3d5bf32e73f5eedcd
Abjura <bryamccuervo2004@gmail.com>
2024-08-11 18:36:48 -0500

    Color en el texto del login

--- codigo/MrHomero-React/src/Pages/default/Ingresar.jsx
+++ codigo/MrHomero-React/src/Pages/default/Ingresar.jsx
@@ -10,11 +10,7 @@ export default function Ingresar() {
   return (
     <div className="">
       <NavegacionDefault />
-<<<<<<< HEAD
       <div className='container-fluid p-5 my-5 text-center border wipe-in-down' transition-style="in:wipe:up" style={{ borderRadius: '20px', width: '85%', boxShadow: '0 0 15px 0 rgba(0, 0, 0, 0.3)' }}>
-=======
-      <div className='container-fluid p-5 my-5 text-center border wipe-in-down' transition-style="in:circle:bottom-left" style={{ borderRadius: '20px', width: '85%', boxShadow: '0 0 15px 0 rgba(0, 0, 0, 0.3)' }}>
->>>>>>> e4e0345ca92af3276c90bb97b3ede8c02c073bfa
         <div className="row">
           <div className="col-12 col-sm-5 align-content-center align-items-center p-5" style={{ boxShadow: '0 0 10px 0 rgb(0, 0, 0, 0.2)' }}>
             <i className='display-1 bi bi-person-circle'></i>
@@ -47,23 +43,11 @@ export default function Ingresar() {
               <p className="text-center text-secondary"><small> ¿No tienes una cuenta? <Link to="/registrar">Registrarte</Link> </small></p>
             </div>
           </div>
-<<<<<<< HEAD
           <div className="d-none d-sm-flex col-sm-7">
             <div className="my-auto text-center w-100">
               <Spline />
-              <h1 className='text-end'>Mr. Homero</h1>
-              <p className='text-end'>Si lo que buscas es sabor Mr. Homero es el mejor</p>
-=======
-          <div className="d-none d-sm-flex col-sm-7 text-center homero-font">
-            <div className="row">
-              <div className="col-12">
-                <h1 className='display-4 text-warning'>Mr. Homero</h1>
-                <p className='fs-3'>¡Si lo que buscas es sabor Mr. Homero es el mejor!</p>
-              </div>
-              <div className="col-12">
-                <Spline />
-              </div>
->>>>>>> e4e0345ca92af3276c90bb97b3ede8c02c073bfa
+              <h1 className='text-end homero-font fs-1 text-warning'>Mr. Homero</h1>
+              <p className='text-end homero-font fs-5'>Si lo que buscas es sabor Mr. Homero es el mejor</p>
             </div>
           </div>
         </div>
