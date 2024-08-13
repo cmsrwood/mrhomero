0acb631783ccef6661aa637bcfe064f10a1f53dd
johansmc <163949679+johansmc@users.noreply.github.com>
2024-08-13 12:49:33 -0500

    Se agregó un modal para gestionar empleado

--- codigo/MrHomero-React/src/Pages/admin/Empleados.jsx
+++ codigo/MrHomero-React/src/Pages/admin/Empleados.jsx
@@ -5,13 +5,60 @@ import Swal from 'sweetalert2'
 export default function Empleados() {
   function Empleado() {
     return (
-      <div className="card">
+      <div className="card p-2">
         <img src={img} height={200} className="card-img-top" alt="..." />
         <div className="card-body">
           <h3 className="card-title">Empleado</h3>
         </div>
         {/* boton de gestionar empleado*/}
-        <button type="button" className="btn btn-outline-warning ms-2" data-bs-toggle="modal" data-bs-target="#Añadir"><i className="bi bi-plus-circle"></i> Gestionar</button>
+        <button type="button" className="btn btn-outline-warning ms-2" data-bs-toggle="modal" data-bs-target="#Gestionar"><i className="bi bi-plus-circle"></i> Gestionar</button>
+        {/* Modal para gestionar Empleados */}
+        <div className="modal fade" id="Gestionar" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog  modal-xl">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">Editar información Empleado</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <div className="row">
+                  <div className="col-3">
+                    <img src={img} height={200} className="card-img-top" alt="..." />
+                  </div>
+                  <div className="col row" >
+                    <div className="col">
+                      <label htmlFor="floatingInput">Nombre</label>
+                      <input type="text" className="form-control my-2" placeholder="Nombre" aria-label="Username" aria-describedby="basic-addon1"></input>
+                      <label htmlFor="floatingInput">Telefono</label><input type="text" className="form-control my-2" placeholder="Telefono" aria-label="Username" aria-describedby="basic-addon1"></input>
+                    </div>
+                    <div className="col">
+                      <label htmlFor="floatingInput">Email</label>
+                      <input type="text" className="form-control my-2" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1"></input>
+                      <label htmlFor="floatingInput">Numero de documento</label>
+                      <input type="text" className="form-control my-2" placeholder="Numero de documento" aria-label="Username" aria-describedby="basic-addon1"></input>
+                    </div>
+                    <div className="mt-2">
+                      <label htmlFor="floatingInput">Fecha de ingreso</label>
+                      <input disabled type="date" name="" id="" className="form-control my-2" />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-outline-danger" data-bs-dismiss="modal">Cancelar</button>
+                  <button type="button" className="btn btn-outline-warning" onClick={() => {
+                    Swal.fire({
+                      title: 'Empleado editado correctamente.',
+                      text: 'El empleado fue editado correctamente.',
+                      icon: 'success',
+                      confirmButtonText: 'Hecho'
+                    })
+                  }}>Guardar cambios</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
