8a33ff52b481a0397552ee3c6b835949571dfccd
Abjura <bryamccuervo2004@gmail.com>
2024-08-14 20:03:46 -0500

    Se añaden estilos a el correo :D

--- codigo/backend/controllers/authController.js
+++ codigo/backend/controllers/authController.js
@@ -125,13 +125,19 @@ exports.recuperar = (req, res) => {
                 to: email,
                 subject: 'Código de verificación para restablecer contraseña || Mr. Homero',
                 html: `
+                <div class="container" style="background-color: #212529; color: #fff; padding: 80px;">
+                    <div class="imagen" style="text-align: center;">
+                        <img src="https://mrhomero.onrender.com/logo.png" alt="https://mrhomero.onrender.com/logo.png"
+                            style="width: 20%; height: 20%;">
+                    </div>
                     <h1>Recuperación de Contraseña</h1>
                     <p>Tu código de verificación es:</p>
-                    <h2 style="font-size: 24px; font-weight: bold;">${verificationCode}</h2>
+                    <h2 style="font-size: 24px; font-weight: bold; color: #FFC107;">${verificationCode}</h2>
                     <p>Por favor, ingrésalo en el formulario de recuperación de contraseña.</p>
                     <p>Si no solicitaste este cambio, ignora este mensaje.</p>
                     <p>Gracias,</p>
                     <p>El equipo de soporte</p>
+                </div>
                 `,
             };
 
