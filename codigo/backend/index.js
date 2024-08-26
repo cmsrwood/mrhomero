f0f396524422b64e0a0feff6c7fea38f858bcfe9
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-08-26 13:28:33 -0500

    origin

--- codigo/backend/index.js
+++ codigo/backend/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 const secret = 'mysecretkey';
 
 app.use(cors({
-    origin: '*',
+    origin: 'https://mrhomero.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
