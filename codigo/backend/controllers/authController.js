6585e30b0d3730425f00600c5908183157fa5b59
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-08-26 13:04:39 -0500

    configuracion para usuario y contraseña de los correos

--- codigo/backend/controllers/authController.js
+++ codigo/backend/controllers/authController.js
@@ -11,8 +11,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-        user: 'dilanfantas@gmail.com',
-        pass: 'jegc hedq jngv tyyg'
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
 });
 
