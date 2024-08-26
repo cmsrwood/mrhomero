7e3fd2a06dcf1c7a3771d7b2db9257f44175afe4
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-08-26 13:54:15 -0500

    recuperar

--- codigo/frontend/src/Pages/default/EmailRecuperar.jsx
+++ codigo/frontend/src/Pages/default/EmailRecuperar.jsx
@@ -21,7 +21,7 @@ export default function Recuperar() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post(`${BACKEND_URL}/auth/recuperarw`, userEmailRecuperar);
+            const res = await axios.post(`${BACKEND_URL}/auth/recuperar`, userEmailRecuperar);
             if (res.status === 200) {
                 Swal.fire({
                     title: 'Email enviado',
