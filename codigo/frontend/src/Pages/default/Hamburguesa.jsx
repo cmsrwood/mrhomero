5026749f2cb4088cb17ec6a5f2776cc13b514b32
Abjuraa <107774277+Abjuraa@users.noreply.github.com>
2024-08-27 10:41:13 -0500

    comentario

--- codigo/frontend/src/Pages/default/Hamburguesa.jsx
+++ codigo/frontend/src/Pages/default/Hamburguesa.jsx
@@ -1,5 +1,6 @@
 import Spline from '@splinetool/react-spline';
 
+{/*Imagen de hamburguesa*/}
 export default function App() {
     return (
         <Spline scene="https://prod.spline.design/hLbXzmKOd-8IiukC/scene.splinecode" />
