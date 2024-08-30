e65a05c703149d6d01a1eac89c1391d04382355a
Abjuraa <107774277+Abjuraa@users.noreply.github.com>
2024-08-30 11:02:52 -0500

    recompensas admin se agrego navbar y se removio el padding superior

--- codigo/frontend/src/Pages/admin/RecompensasAdmin.jsx
+++ codigo/frontend/src/Pages/admin/RecompensasAdmin.jsx
@@ -7,6 +7,7 @@ import 'swiper/css/scrollbar'
 import '../../styles/style.css'
 import Swal from 'sweetalert2'
 import img from '../../assets/img/img.png'
+import NavegacionAdmin from '../../navigation/NavegacionAdmin'
 
 export default function RecompensasAdmin() {
   function rcard() {
@@ -43,146 +44,149 @@ export default function RecompensasAdmin() {
     )
   }
   return (
-    <div className="container p-5">
-      <div className="d-flex justify-content-between mt-5">
-        <h1>Recompensas</h1>
-        <button type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#client_add"><i className="bi bi-plus"></i>Añadir</button>
+    <div>
+      <NavegacionAdmin/>
+      <div className="container">
+        <div className="d-flex justify-content-between mt-5">
+          <h1>Recompensas</h1>
+          <button type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#client_add"><i className="bi bi-plus"></i>Añadir</button>
 
-        <div className="modal fade" id="client_add" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-          <div className="modal-dialog modal-xl">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h1 className="modal-title fs-5" id="staticBackdropLabel">Añadir recompensa</h1>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div className="modal-body">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-3 m-3 ps-3 pt-2">
-                      <img src={img} height={250} className='card-img-center border' alt="..." />
-                      <input type="file" className='form-control mt-5' accept='image/*'></input>
-                    </div>
-                    <div className="col">
-                      <label htmlFor="floatingInput">Nombre</label>
-                      <input type="text" className="form-control my-2" placeholder="Nombre" />
-                      <label htmlFor="floatingInput">Tipo</label>
-                      <select type="text" className="form-select my-2" placeholder="Tipo">
-                        <option disabled selected>Tipo</option>
-                        <option>Tipo</option>
-                        <option>Tipo</option>
-                        <option>Tipo</option>
-                        <option>Tipo</option>
-                      </select>
-                      <label htmlFor="floatingInput">Puntos</label>
-                      <input type="text" className="form-control my-2" placeholder="Puntos" />
-                      <label htmlFor="floatingInput">Descripcion</label>
-                      <textarea className="form-control my-2" placeholder="Descripción" id="floatingTextarea"></textarea>
+          <div className="modal fade" id="client_add" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div className="modal-dialog modal-xl">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h1 className="modal-title fs-5" id="staticBackdropLabel">Añadir recompensa</h1>
+                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-3 m-3 ps-3 pt-2">
+                        <img src={img} height={250} className='card-img-center border' alt="..." />
+                        <input type="file" className='form-control mt-5' accept='image/*'></input>
+                      </div>
+                      <div className="col">
+                        <label htmlFor="floatingInput">Nombre</label>
+                        <input type="text" className="form-control my-2" placeholder="Nombre" />
+                        <label htmlFor="floatingInput">Tipo</label>
+                        <select type="text" className="form-select my-2" placeholder="Tipo">
+                          <option disabled selected>Tipo</option>
+                          <option>Tipo</option>
+                          <option>Tipo</option>
+                          <option>Tipo</option>
+                          <option>Tipo</option>
+                        </select>
+                        <label htmlFor="floatingInput">Puntos</label>
+                        <input type="text" className="form-control my-2" placeholder="Puntos" />
+                        <label htmlFor="floatingInput">Descripcion</label>
+                        <textarea className="form-control my-2" placeholder="Descripción" id="floatingTextarea"></textarea>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-danger" data-bs-dismiss="modal"><i className="bi bi-x-circle"></i></button>
-                  <button type="button" className="btn btn-success" onClick={() => {
-                    Swal.fire({
-                      icon: 'question',
-                      title: '¿Desea crear una nueva recompensa?',
-                      showCancelButton: true,
-                      confirmButtonText: 'Si, confirmar',
-                      confirmButtonColor: '#3085d6',
-                      cancelButtonText: 'Cancelar',
-                      cancelButtonColor: '#d33'
-                    }).then((result) => {
-                      if (result.isConfirmed) {
-                        Swal.fire({
-                          icon: 'success',
-                          title: '¡Se ha creado una nueva recompensa!'
-                        })
-                      }
-                    })
-                  }}><i className="bi bi-check-circle"></i></button>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-danger" data-bs-dismiss="modal"><i className="bi bi-x-circle"></i></button>
+                    <button type="button" className="btn btn-success" onClick={() => {
+                      Swal.fire({
+                        icon: 'question',
+                        title: '¿Desea crear una nueva recompensa?',
+                        showCancelButton: true,
+                        confirmButtonText: 'Si, confirmar',
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonText: 'Cancelar',
+                        cancelButtonColor: '#d33'
+                      }).then((result) => {
+                        if (result.isConfirmed) {
+                          Swal.fire({
+                            icon: 'success',
+                            title: '¡Se ha creado una nueva recompensa!'
+                          })
+                        }
+                      })
+                    }}><i className="bi bi-check-circle"></i></button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="mt-3">
-        <Swiper
-          slidesPerView={4}
-          spaceBetween={10}
-          pagination={{
-            clickable: true,
-          }}
-          scrollbar={{ hide: true }}
-          modules={[Scrollbar]}
-        >
+        <div className="mt-3">
+          <Swiper
+            slidesPerView={4}
+            spaceBetween={10}
+            pagination={{
+              clickable: true,
+            }}
+            scrollbar={{ hide: true }}
+            modules={[Scrollbar]}
+          >
 
-          <SwiperSlide>{rcard()}</SwiperSlide>
-          <SwiperSlide>{rcard()}</SwiperSlide>
-          <SwiperSlide>{rcard()}</SwiperSlide>
-          <SwiperSlide>{rcard()}</SwiperSlide>
-          <SwiperSlide>{rcard()}</SwiperSlide>
-          <SwiperSlide>{rcard()}</SwiperSlide>
-          <SwiperSlide>{rcard()}</SwiperSlide>
-          <SwiperSlide>{rcard()}</SwiperSlide>
-          <SwiperSlide>{rcard()}</SwiperSlide>
-          <SwiperSlide>{rcard()}</SwiperSlide>
-        </Swiper>
-      </div>
-      <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div className="modal-dialog modal-xl">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="staticBackdropLabel">Editar información</h1>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
-              <div className="conatiner">
-                <div className="row">
-                  <div className="col-3 m-3 ps-3 pt-2">
-                    <img src={img} height={250} className="card-img-center border" alt="..." />
-                    <input type="file" className="form-control mt-5" accept='image/*' />
-                  </div>
-                  <div className="col">
-                    <label htmlFor="floatingInput">Nombre</label>
-                    <input type="text" className="form-control my-2" placeholder="Nombre" aria-label="Username" aria-describedby="basic-addon1"></input>
-                    <label htmlFor="floatingInput">Tipo</label>
-                    <select type="text" className="form-control my-2" placeholder="Tipo" aria-label="Username" aria-describedby="basic-addon1">
-                      <option>Premio</option>
-                      <option>Premio</option>
-                      <option>Premio</option>
-                      <option>Premio</option>
-                      <option>Premio</option>
-                    </select>
-                    <label htmlFor="floatingInput">Puntos</label>
-                    <input type="number" className="form-control my-2" placeholder="Puntos" aria-label="Username" aria-describedby="basic-addon1"></input>
-                    <label htmlFor="floatingInput">Descripción</label>
-                    <textarea className='form-control' id="descripcion" placeholder='Inserte la descripcion'></textarea>
+            <SwiperSlide>{rcard()}</SwiperSlide>
+            <SwiperSlide>{rcard()}</SwiperSlide>
+            <SwiperSlide>{rcard()}</SwiperSlide>
+            <SwiperSlide>{rcard()}</SwiperSlide>
+            <SwiperSlide>{rcard()}</SwiperSlide>
+            <SwiperSlide>{rcard()}</SwiperSlide>
+            <SwiperSlide>{rcard()}</SwiperSlide>
+            <SwiperSlide>{rcard()}</SwiperSlide>
+            <SwiperSlide>{rcard()}</SwiperSlide>
+            <SwiperSlide>{rcard()}</SwiperSlide>
+          </Swiper>
+        </div>
+        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+          <div className="modal-dialog modal-xl">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="staticBackdropLabel">Editar información</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <div className="conatiner">
+                  <div className="row">
+                    <div className="col-3 m-3 ps-3 pt-2">
+                      <img src={img} height={250} className="card-img-center border" alt="..." />
+                      <input type="file" className="form-control mt-5" accept='image/*' />
+                    </div>
+                    <div className="col">
+                      <label htmlFor="floatingInput">Nombre</label>
+                      <input type="text" className="form-control my-2" placeholder="Nombre" aria-label="Username" aria-describedby="basic-addon1"></input>
+                      <label htmlFor="floatingInput">Tipo</label>
+                      <select type="text" className="form-control my-2" placeholder="Tipo" aria-label="Username" aria-describedby="basic-addon1">
+                        <option>Premio</option>
+                        <option>Premio</option>
+                        <option>Premio</option>
+                        <option>Premio</option>
+                        <option>Premio</option>
+                      </select>
+                      <label htmlFor="floatingInput">Puntos</label>
+                      <input type="number" className="form-control my-2" placeholder="Puntos" aria-label="Username" aria-describedby="basic-addon1"></input>
+                      <label htmlFor="floatingInput">Descripción</label>
+                      <textarea className='form-control' id="descripcion" placeholder='Inserte la descripcion'></textarea>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-danger" data-bs-dismiss="modal"><i className="bi bi-x-circle"></i></button>
-              <button type="button" className="btn btn-success" onClick={() => {
-                Swal.fire({
-                  icon: 'question',
-                  title: '¿Desea guardar los cambios?',
-                  showCancelButton: true,
-                  confirmButtonColor: '#3085d6',
-                  cancelButtonColor: '#d33',
-                  confirmButtonText: 'Si, acepto',
-                  cancelButtonText: 'Cancelar',
-                }).then((result) => {
-                  if (result.isConfirmed) {
-                    Swal.fire(
-                      '¡Guardado!',
-                      'Los cambios fueron guardados correctamente.',
-                      'success'
-                    )
-                  }
-                })
-              }}><i className="bi bi-check2-square"></i></button>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-danger" data-bs-dismiss="modal"><i className="bi bi-x-circle"></i></button>
+                <button type="button" className="btn btn-success" onClick={() => {
+                  Swal.fire({
+                    icon: 'question',
+                    title: '¿Desea guardar los cambios?',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Si, acepto',
+                    cancelButtonText: 'Cancelar',
+                  }).then((result) => {
+                    if (result.isConfirmed) {
+                      Swal.fire(
+                        '¡Guardado!',
+                        'Los cambios fueron guardados correctamente.',
+                        'success'
+                      )
+                    }
+                  })
+                }}><i className="bi bi-check2-square"></i></button>
+              </div>
             </div>
           </div>
         </div>
