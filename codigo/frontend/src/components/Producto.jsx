be20d8fc98612f98735e86f8fd0efc3c38869c2e
Abjura <107774277+Abjuraa@users.noreply.github.com>
2024-09-09 18:46:30 -0500

    Productos casi terminado

--- codigo/frontend/src/components/Producto.jsx
+++ codigo/frontend/src/components/Producto.jsx
@@ -5,15 +5,60 @@ import "../styles/product.css";
 export default function Producto() {
     return (
         <>
-            <div class="content">
-                <div class="img-product">
+            <div className="content">
+                <div className="img-product">
                     <img src={img} alt="" />
                 </div>
-                <div class="content-product">
+                <div className="content-product">
                     <h1>Hamburguesa</h1>
                     <h2>Categoria: Hamburguesa</h2>
-                    <h2>descripcion: Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illum dignissimos reiciendis, quae quas eligendi alias, ab consequatur amet recusandae reprehenderit! Officiis deserunt, est reiciendis porro magni enim quod dolorem.</h2>
+                    <h2>Descripcion: Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illum dignissimos reiciendis, quae quas eligendi alias, ab consequatur amet recusandae reprehenderit! Officiis deserunt, est reiciendis porro magni enim quod dolorem.</h2>
                     <h2>Precio: $10.000</h2>
+                </div>
+            </div>
+            <div className="suggestion">
+                <h4 className="title-suggestion">Tambien te puede gustar</h4>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <div className="suggestion-card">
+                            <img src={img} alt="" />
+                            <div className="content-suggestion">
+                                <h4>Hamburguesa</h4>
+                                <p>descripcion: Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+                                <p>Precio: $10.000</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-6">
+                        <div className="suggestion-card">
+                            <img src={img} alt="" />
+                            <div className="content-suggestion">
+                                <h4>Hamburguesa</h4>
+                                <p>descripcion: Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+                                <p>Precio: $10.000</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-6">
+                        <div className="suggestion-card">
+                            <img src={img} alt="" />
+                            <div className="content-suggestion">
+                                <h4>Hamburguesa</h4>
+                                <p>descripcion: Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+                                <p>Precio: $10.000</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-6">
+                        <div className="suggestion-card">
+                            <img src={img} alt="" />
+                            <div className="content-suggestion">
+                                <h4>Hamburguesa</h4>
+                                <p>descripcion: Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+                                <p>Precio: $10.000</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
