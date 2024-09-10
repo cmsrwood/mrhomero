98be01dfbbc980135f430899d84ef8105320c6e8
Abjura <107774277+Abjuraa@users.noreply.github.com>
2024-09-09 20:41:42 -0500

    Pagina de productos terminado

--- codigo/frontend/src/components/Producto.jsx
+++ codigo/frontend/src/components/Producto.jsx
@@ -3,6 +3,23 @@ import img from "../assets/img/hamburguesa.png";
 import "../styles/product.css";
 
 export default function Producto() {
+
+    function card() {
+        return (
+            <div className="col-sm-6">
+                <div className="suggestion-card">
+                    <img src={img} alt="" />
+                    <div className="content-suggestion">
+                        <h4>Hamburguesa</h4>
+                        <p style={{maxHeight: "150px", overflowY: "auto" }}>descripcion: Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    </div>
+                    <p className="price">$10.000</p>
+
+                </div>
+            </div>
+        )
+    }
+
     return (
         <>
             <div className="card-product-content">
@@ -13,52 +30,16 @@ export default function Producto() {
                     <h1>Hamburguesa</h1>
                     <h2>Categoria: Hamburguesa</h2>
                     <h2>Descripcion: Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illum dignissimos reiciendis, quae quas eligendi alias, ab consequatur amet recusandae reprehenderit! Officiis deserunt, est reiciendis porro magni enim quod dolorem.</h2>
-                    <h2>Precio: $10.000</h2>
+                    <h2 className="price">$10.000</h2>
                 </div>
             </div>
             <div className="suggestion">
                 <h4 className="title-suggestion">Tambien te puede gustar</h4>
                 <div className="row">
-                    <div className="col-sm-6">
-                        <div className="suggestion-card">
-                            <img src={img} alt="" />
-                            <div className="content-suggestion">
-                                <h4>Hamburguesa</h4>
-                                <p>descripcion: Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
-                                <p>Precio: $10.000</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-6">
-                        <div className="suggestion-card">
-                            <img src={img} alt="" />
-                            <div className="content-suggestion">
-                                <h4>Hamburguesa</h4>
-                                <p>descripcion: Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
-                                <p>Precio: $10.000</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-6">
-                        <div className="suggestion-card">
-                            <img src={img} alt="" />
-                            <div className="content-suggestion">
-                                <h4>Hamburguesa</h4>
-                                <p>descripcion: Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
-                                <p>Precio: $10.000</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-6">
-                        <div className="suggestion-card">
-                            <img src={img} alt="" />
-                            <div className="content-suggestion">
-                                <h4>Hamburguesa</h4>
-                                <p>descripcion: Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
-                                <p>Precio: $10.000</p>
-                            </div>
-                        </div>
-                    </div>
+                    {card()}
+                    {card()}
+                    {card()}
+                    {card()}
                 </div>
             </div>
         </>
