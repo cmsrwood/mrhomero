a3a1b210054c4d5142190df531d6e66e37c400ab
cmsrwood <dilanfantas@gmail.com>
2024-09-26 16:04:20 -0500

    proveedores

--- codigo/frontend/src/Pages/admin/Proveedores.jsx
+++ codigo/frontend/src/Pages/admin/Proveedores.jsx
@@ -2,40 +2,40 @@ import React from 'react'
 import NavegacionAdmin from '../../navigation/NavegacionAdmin'
 
 export default function Proveedores() {
-  return (
-    <div className='d-flex'>
-        <NavegacionAdmin />
-        <div className="container">
-              <table class="table">
-                  <thead>
-                      <tr>
-                          <th scope="col">#</th>
-                          <th scope="col">First</th>
-                          <th scope="col">Last</th>
-                          <th scope="col">Handle</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <tr>
-                          <th scope="row">1</th>
-                          <td>Mark</td>
-                          <td>Otto</td>
-                          <td>@mdo</td>
-                      </tr>
-                      <tr>
-                          <th scope="row">2</th>
-                          <td>Jacob</td>
-                          <td>Thornton</td>
-                          <td>@fat</td>
-                      </tr>
-                      <tr>
-                          <th scope="row">3</th>
-                          <td colspan="2">Larry the Bird</td>
-                          <td>@twitter</td>
-                      </tr>
-                  </tbody>
-              </table>
+    return (
+        <div className='d-flex'>
+            <NavegacionAdmin />
+            <div className="container">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
