cca1c17746cc9c47ddbfcc82e78fdff9a97a96be
johansmc <163949679+johansmc@users.noreply.github.com>
2024-10-03 12:46:28 -0500

    Update Menu.jsx

--- codigo/frontend/src/components/Menu.jsx
+++ codigo/frontend/src/components/Menu.jsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y, EffectCreative } from 'swiper/modules';
 import 'swiper/css/bundle';
@@ -7,14 +8,33 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import 'swiper/css/effect-creative';
 import img from '../assets/img/img.png';
+import axios from 'axios';
+const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:4400";
 
 export default function Menu() {
+
+    const [categorias, setCategorias] = useState([])
+    const [isDataUpdated, setIsDataUpdated] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const [categoriasRes] = await Promise.all([
+                    axios.get(`${BACKEND_URL}/menu/mostrarCategorias`),
+                ]);
+                setCategorias(categoriasRes.data);
+            } catch (error) {
+                console.log(error);
+            }
+            setIsDataUpdated(false);
+        };
+        fetchData();
+    }, [isDataUpdated]);
 
     return (
         <div className="">
             <div className="container">
                 <h1 className="text-warning text-center p-2">Menu</h1>
-                <h2 className="p-2 mb-4">Categoria</h2>
                 <Swiper
                     grabCursor={true}
                     effect={'creative'}
@@ -41,16 +61,18 @@ export default function Menu() {
                     }}
                     className="mySwiper2"
                 >
-                    <SwiperSlide>
-                        <div className="card text-center">
-                            <img src={img} height={200} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Hamburguesa</h5>
-                                <p className="card-text">$10.000</p>
-                                <a href="#" className="btn btn-primary">Ver</a>
-                            </div>
-                        </div>
-                    </SwiperSlide>
+                    {categorias.map((categoria) => (
+                        <SwiperSlide key={categoria.id_categoria}>
+                            <Link to={`/menu/${categoria.id_categoria}`} style={{ textDecoration: 'none' }}>
+                                <div className="card text-center border-0">
+                                    <img src={`/images/menu/categorias/${categoria.cat_foto}`} height={200} className="card-img-top" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{categoria.cat_nom}</h5>
+                                    </div>
+                                </div>
+                            </Link>
+                        </SwiperSlide>
+                    ))}
                 </Swiper>
             </div>
         </div>
