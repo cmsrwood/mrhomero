cd8dc3ac3529009748f14e19bf2cb35444055dd0
cmsrwood <114179857+cmsrwood@users.noreply.github.com>
2024-11-18 19:49:47 -0500

    onSubmit menu admin

--- codigo/frontend/src/Pages/admin/MenuAdmin.jsx
+++ codigo/frontend/src/Pages/admin/MenuAdmin.jsx
@@ -181,7 +181,7 @@ export default function MenuAdmin() {
               </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                <button type="button" className="btn btn-success">Guardar</button>
+                <button type="submit" className="btn btn-success">Guardar</button>
               </div>
             </form>
           </div>
