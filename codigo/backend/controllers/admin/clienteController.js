757d08fef9ccc5ba563f35d385f94f734afee4ed
johansmc <163949679+johansmc@users.noreply.github.com>
2024-11-27 17:26:41 -0500

    Editar perfil usuarios en proceso
    Co-Authored-By: Abjura <107774277+Abjuraa@users.noreply.github.com>

--- codigo/backend/controllers/admin/clienteController.js
+++ codigo/backend/controllers/admin/clienteController.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         const ext = file.originalname.split(".").pop();
-        cb(null, `recompensa_${Date.now()}.${ext}`);
+        cb(null, `cliente_${Date.now()}.${ext}`);
     }
 });
 
@@ -28,37 +28,53 @@ const eliminar = async (image) => {
 };
 exports.actualizarCliente = (req, res) => {
     const id = req.params.id;
-    const nombre = req.body.cliente_nombre;
-    const apellido = req.body.cliente_apellido;
-    const email = req.body.cliente_email;
-    const telefono = req.body.cliente_telefono;
+    const nombre = req.body.user_nom;
+    const apellido = req.body.user_apels;
+    const email = req.body.user_email;
+    const telefono = req.body.user_tel;
+    const file = req.file || null;
 
-    db.query('SELECT * FROM usuarios WHERE id_user = ?', [id], (err, results) => {
+    const qCliente = 'SELECT * FROM usuarios WHERE id_user = ?'
+    db.query(qCliente, [id], (err, results) => {
         if (err) {
             console.log(err);
             return res.status(500).send('Error en el servidor');
         }
-        else {
-            if (results.length === 0) {
-                return res.status(404).send('Cliente no encontrado');
-            }
-            else {
-                db.query('UPDATE usuarios SET user_nom = ?, user_apels = ?, user_email = ?, user_tel = ? WHERE id_user = ?', [nombre, apellido, email, telefono, id], (err) => {
-                    if (err) {
-                        console.log(err);
-                        return res.status(500).send('Error en el servidor');
-                    }
-                    else {
-                        return res.status(200).send('Cliente actualizado exitosamente');
-                    }
-                });
-            }
+
+        const clienteActual = results[0];
+        const nombreActualizado = nombre || clienteActual.usuario_nombre;
+        const apellidoActualizado = apellido || clienteActual.usuario_apellidos;
+        const emailActualizado = email || clienteActual.usuario_email;
+        const telefonoActualizado = telefono || clienteActual.usuario_telefono;
+        const fotoActualizada = file ? file.filename.toString() : clienteActual.user_foto;
+
+        if (file && clienteActual.user_foto) {
+            eliminar(clienteActual.user_foto);
         }
-    }
-    );
+
+        const q = 'UPDATE usuarios SET user_nom = ?, user_apels = ?, user_email = ?, user_tel = ?, user_foto = ? WHERE id_user = ?';
+
+        const values = [
+            nombreActualizado,
+            apellidoActualizado,
+            emailActualizado,
+            telefonoActualizado,
+            fotoActualizada,
+            id
+        ];
+
+        db.query(q, values, (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send('Error al editar el usuario' + err.message);
+            } else {
+                return res.status(200).send('Cliente actualizado exitosamente');
+            }
+        })
+    })
 }
 exports.upload = upload.single('foto');
-    
+
 
 exports.mostrarClientes = (req, res) => {
     db.query('SELECT * FROM usuarios WHERE id_rol = 3', (err, results) => {
--- codigo/frontend/src/Pages/cliente/PerfilCliente.jsx
+++ codigo/frontend/src/Pages/cliente/PerfilCliente.jsx
@@ -11,13 +11,27 @@ export default function PerfilCliente() {
   const token = localStorage.getItem('token');
   const id_user = JSON.parse(atob(token.split(".")[1])).id;
 
-  const [user, setUser] = useState([]);
-  const [compras , setCompras] = useState([]);
+  const [user, setUser] = useState({
+    user_nom: '',
+    user_apels: '',
+    user_email: '',
+    user_tel: '',
+    user_foto: null,
+  });
+  const [compras, setCompras] = useState([]);
+  
+  const [editarUser, setEditarUser] = useState({
+    user_nom: '',
+    user_apels: '',
+    user_email: '',
+    user_tel: '',
+    user_foto: null,
+  });
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [userRes, comprasRes ] = await Promise.all([
+        const [userRes, comprasRes] = await Promise.all([
           axios.get(`${BACKEND_URL}/api/clientes/mostrarByid/${id_user}`),
           axios.get(`${BACKEND_URL}/api/ventas/mostrarCompras/${id_user}`),
         ])
@@ -32,62 +46,108 @@ export default function PerfilCliente() {
   }, [isDataUpdated, id_user]);
 
   const handleChange = (e) => {
-    setUser({ ...user, [e.target.name]: e.target.value });
+    setEditarUser({ ...editarUser, [e.target.name]: e.target.value });
     setIsDataUpdated(true);
   };
+
+  const handleFileChange = (e) => {
+    setEditarUser({ ...editarUser, user_foto: e.target.files[0] });
+    setIsDataUpdated(true);
+  };
+
+
+  const handleEdit = async (id) => {
+    const formData = new FormData();
+    formData.append('usuario_nombre', editarUser.user_nom);
+    formData.append('usuario_apellidos', editarUser.user_apels);
+    formData.append('usuario_email', editarUser.user_email);
+    formData.append('usuario_telefono', editarUser.user_tel);
+
+    if (editarUser.user_foto) {
+      formData.append('foto', editarUser.user_foto);
+    }
+
+    try {
+      const res = await axios.put(`${BACKEND_URL}/api/clientes/actualizar/${id}`, formData);
+      if (res.status === 200) {
+        Swal.fire('Exito', 'Cliente editado correctamente', 'success');
+        setIsDataUpdated(true);
+      }
+    } catch (error) {
+      console.log(error);
+      Swal.fire('Error', error.response?.data || 'error');
+    }
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('user_nom', user.user_nom);
+    formData.append('user_apels', user.user_apels);
+    formData.append('user_email', user.user_email);
+    formData.append('user_tel', user.user_tel);
+    formData.append('user_foto', user.user_foto);
+    try {
+      const res = await axios.put(`${BACKEND_URL}/api/clientes/actualizar/${id_user}`, formData);
+      if (res.status === 200) {
+        Swal.fire({
+          title: 'Guardado',
+          text: 'Se han guardado los cambios',
+          icon: 'success',
+          confirmButtonText: 'Hecho'
+        });
+        setIsDataUpdated(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div>
       <div className="px-5">
-        <div className='align-items-center text-center pb-3 pt-3'>
-          <img src={img} height={300} alt="" className='rounded rounded-circle border border-3 px-3' />
-          <input type="file" className="form-control my-3" id="inputGroupFile01" />
-          <h1>{user.user_nom} {user.user_apels}</h1>
-          <div className="row">
-            <div className="col-6">
-              <h2>Compras realizadas</h2>
-              <h3>{compras.length}</h3>
+        <form action="" >
+          <div className='align-items-center text-center pb-3 pt-3'>
+            <img src={`/images/clientes/${user.user_foto}`} height={300} alt="" className='rounded rounded-circle border border-3 px-3' />
+            <input type="file" className="form-control my-3" id="inputGroupFile01" onChange={handleFileChange}  name='user_foto'/>
+            <h1>{user.user_nom} {user.user_apels}</h1>
+            <div className="row">
+              <div className="col-6">
+                <h2>Compras realizadas</h2>
+                <h3>{compras.length}</h3>
+              </div>
+              <div className="col-6">
+                <h2>Puntos acumulados</h2>
+                <h3>{user.user_puntos}</h3>
+              </div>
             </div>
-            <div className="col-6">
-              <h2>Puntos acumulados</h2>
-              <h3>{user.user_puntos}</h3>
+            <hr className='border border-3' />
+          </div>
+          <div className='fs-5' key={user.id_user}>
+            <h1> Información personal</h1>
+            <hr className='border border-3' />
+            <div className="row cols-2">
+              <div className="col-6">
+                <label className='form-label '>Nombre</label>
+                <input type="text" className="form-control" onChange={handleChange} name="user_nom" placeholder={user.user_nom} value={editarUser.user_nom} required/>
+              </div>
+              <div className="col-6">
+                <label className='form-label '>Apellidos</label>
+                <input type="text" className="form-control" onChange={handleChange} name="user_apels" placeholder={user.user_apels} value={editarUser.user_apels} required />
+              </div>
+              <div className="col-6">
+                <label className='form-label '>Email</label>
+                <input type="text" className="form-control" onChange={handleChange} name="user_email" placeholder={user.user_email}  value={editarUser.user_email} required/>
+              </div>
+              <div className="col-6 mt-2">
+                <label className='form-label '>Telefono</label>
+                <input type="text" className="form-control" onChange={handleChange} name="user_tel" placeholder={user.user_tel} value={editarUser.user_tel}  required/>
+              </div>
+              <button type="submit" className="btn btn-warning w-100 mt-3" onClick={() => handleEdit(user.id_user)}>
+                <i className="bi bi-pencil-square"></i> Guardar cambios
+              </button>
             </div>
           </div>
-          <hr className='border border-3' />
-        </div>
-        <div className='fs-5' key={user.id_user}>
-          <h1> Información personal</h1>
-          <hr className='border border-3' />
-          <div className="row cols-2">
-            <div className="col-6">
-              <label className='form-label '>Nombre</label>
-              <input type="text" className="form-control" onChange={handleChange} name="user_nom" placeholder="Nombre" defaultValue={user.user_nom} />
-            </div>
-            <div className="col-6">
-              <label className='form-label '>Apellidos</label>
-              <input type="text" className="form-control" onChange={handleChange} name="user_apels" placeholder="Apellidos" defaultValue={user.user_apels} />
-            </div>
-            <div className="col-6">
-              <label className='form-label '>Email</label>
-              <input type="text" className="form-control" onChange={handleChange} name="user_email" placeholder="Email" defaultValue={user.user_email} />
-            </div>
-            <div className="col-6 mt-2">
-              <label className='form-label '>Telefono</label>
-              <input type="text" className="form-control" onChange={handleChange} name="user_tel" placeholder="Telefono" defaultValue={user.user_tel} />
-            </div>
-            <button type="button" className="btn btn-warning w-100 mt-3" onClick={() => {
-              Swal.fire({
-                title: 'Guardado',
-                text: 'Se han guardado los cambios',
-                icon: 'success',
-                confirmButtonText: 'Hecho'
-              });
-            }}>
-              <i className="bi bi-pencil-square"></i> Guardar cambios
-            </button>
-          </div>
-        </div>
-
+        </form>
       </div>
     </div>
   );
