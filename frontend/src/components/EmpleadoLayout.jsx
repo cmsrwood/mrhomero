215efe309f5664267cf51093ec43e8100af188ac
cmsrwood <dilanfantas@gmail.com>
2025-03-11 19:47:48 -0500

    Suspense fallback

--- frontend/src/components/EmpleadoLayout.jsx
+++ frontend/src/components/EmpleadoLayout.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { Suspense } from "react";
 import NavegacionEmpleado from "../navigation/NavegacionEmpleado";
 import {Outlet} from 'react-router-dom';
-
+import Loader from "./Loader";
 
 const EmpleadoLayout = () => {
     return (
         <div className="d-flex">
             <NavegacionEmpleado />
             <div className='container content'>
-                {<Outlet />}
+                <Suspense fallback={<Loader />}>
+                    {<Outlet />}
+                </Suspense>
             </div>
         </div>
     );
