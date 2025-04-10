e9871e80de70fd7f3af3b102727874971f73e85e
Abjura <107774277+Abjuraa@users.noreply.github.com>
2025-04-09 20:52:00 -0500

    indexCliente
    se hace validación si trae productos o no y muestra un mensaje

--- src/Pages/cliente/IndexCliente.jsx
+++ src/Pages/cliente/IndexCliente.jsx
@@ -114,55 +114,60 @@ export default function IndexCliente() {
 
       <div className="container my-5">
         <h2 className='text-center fw-bold my-5'>Productos más comprados</h2>
-        <div className='row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4 mb-5'>
-          {productosMasVendidos.map((producto, index) => {
-            return (
-              index < 4 &&
-              <div key={producto.id_producto} className={index < 2 ? "col wow animate__animated animate__fadeInLeft" : "col wow animate__animated animate__fadeInRight"}>
-                <div className="card text-center shadow position-relative">
-                  <img
-                    src={`${producto.pro_foto}`}
-                    height={200}
-                    className="card-img-top border-bottom border-1"
-                    alt={producto.pro_foto}
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title fs-sm-5">{producto.pro_nom}</h5>
-                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning text-dark">
-                      {producto.cantidad_vendida ? <span className="fw-bold"><i className="bi bi-fire"></i> {producto.cantidad_vendida}</span> : <span>0</span>}
-                      <span className="visually-hidden">unread messages</span>
-                    </span>
+        {productosMasVendidos.length === 0 ? <h2 className='text-center my-5 fs-5'>No hay productos vendidos</h2> :
+          <div className='row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4 mb-5'>
+            {productosMasVendidos.map((producto, index) => {
+              return (
+                index < 4 &&
+                <div key={producto.id_producto} className={index < 2 ? "col wow animate__animated animate__fadeInLeft" : "col wow animate__animated animate__fadeInRight"}>
+                  <div className="card text-center shadow position-relative">
+                    <img
+                      src={`${producto.pro_foto}`}
+                      height={200}
+                      className="card-img-top border-bottom border-1"
+                      alt={producto.pro_foto}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title fs-sm-5">{producto.pro_nom}</h5>
+                      <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning text-dark">
+                        {producto.cantidad_vendida ? <span className="fw-bold"><i className="bi bi-fire"></i> {producto.cantidad_vendida}</span> : <span>0</span>}
+                        <span className="visually-hidden">unread messages</span>
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>}
+
       </div>
 
       <div className="container my-5">
         <h2 className='text-center fw-bold my-5'>Segun los productos que has comprado</h2>
-        <div className='row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4'>
-          {productosMasComprados.map((producto, index) => {
-            return (
-              index < 4 &&
-              <div key={producto.id_producto} className={index < 2 ? "col wow animate__animated animate__fadeInLeft" : "col wow animate__animated animate__fadeInRight"}>
-                <div className="card text-center shadow position-relative">
-                  <img
-                    src={`${producto.pro_foto}`}
-                    height={200}
-                    className="card-img-top border-bottom border-1"
-                    alt={producto.pro_foto}
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title fs-sm-5">{producto.pro_nom}</h5>
-                    <p>Lo has comprado {producto.cantidad_vendida} veces</p>
+        {productosMasComprados.length === 0 ? <h2 className='text-center my-5 fs-5'>Aún no has comprado productos</h2> :
+
+          <div className='row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4'>
+            {productosMasComprados.map((producto, index) => {
+              return (
+                index < 4 &&
+                <div key={producto.id_producto} className={index < 2 ? "col wow animate__animated animate__fadeInLeft" : "col wow animate__animated animate__fadeInRight"}>
+                  <div className="card text-center shadow position-relative">
+                    <img
+                      src={`${producto.pro_foto}`}
+                      height={200}
+                      className="card-img-top border-bottom border-1"
+                      alt={producto.pro_foto}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title fs-sm-5">{producto.pro_nom}</h5>
+                      <p>Lo has comprado {producto.cantidad_vendida} veces</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>
+        }
       </div>
     </div>
   )
